ff9e8071d48be0714e73f4b15ae3998a428784ba
skorpiondeath <skorpiondeath@hotmail.com>
2017-01-30 18:10:37 +0100

    dipendenze

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 // main.js
 var greetings = require("./greetings.js");
+var text = require('text');
 
-console.log("Chiamo greetings: " + greetings.sayHelloInItalian() );
+console.log("Chiamo greetings: " + greetings.sayHelloInItalian());
+console.log("Criptata: " + text.encode64(greetings.sayHelloInItalian()));
